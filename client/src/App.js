9ea4fc1c63445ffd95f92f6abe0ce27b378fcc13
VinayVamshee <144655152+VinayVamshee@users.noreply.github.com>
2025-02-26 21:35:49 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import IndexPage from "./Components/IndexPage";
 import './Components/style.css';
 import { useEffect } from 'react';
 import ReactGA from 'react-ga4';
 
-const TRACKING_ID = 'G-B2Z721XDXX'; 
+const TRACKING_ID = 'G-B2Z721XDXX'; // Replace with your real GA ID
 ReactGA.initialize(TRACKING_ID);
 
 function AnalyticsTracker() {
   const location = useLocation();
 
   useEffect(() => {
-    if (ReactGA.isInitialized()) {
-      ReactGA.send({ hitType: "pageview", page: location.pathname });
-      console.log(`Analytics sent: ${location.pathname}`);
-    } else {
-      console.warn("Google Analytics not initialized");
-    }
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+    console.log(`Analytics sent: ${location.pathname}`);
   }, [location]);
 
   return null;
@@ -24,14 +20,15 @@ function AnalyticsTracker() {
 
 function App() {
   return (
+    <Router>
       <div className="App">
-     <Router> {/* ✅ Now wrapping the app inside BrowserRouter */}
         <AnalyticsTracker />
         <Routes>
           <Route path="/" element={<IndexPage />} />
+          {/* Add more routes if needed */}
         </Routes>
-   </Router>
       </div>
+    </Router>
   );
 }
 
