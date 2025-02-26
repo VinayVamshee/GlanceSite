7072d7b033fe3b21e38e6f2035b071159cf53a76
VinayVamshee <144655152+VinayVamshee@users.noreply.github.com>
2025-02-26 21:17:03 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
 import IndexPage from "./Components/IndexPage";
 import './Components/style.css'
+import ReactGA from 'react-ga4';
+
+const TRACKING_ID = 'G-B2Z721XDXX';
+ReactGA.initialize(TRACKING_ID);
+
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
+  return null;
+}
+
+
+
 function App() {
   return (
     <div className="App">
+    <AnalyticsTracker />
       <IndexPage/>
     </div>
   );
