57fb400a8312f55ae09f9c6c40a4e8a4341998c7
VinayVamshee <144655152+VinayVamshee@users.noreply.github.com>
2025-02-26 21:20:12 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import IndexPage from "./Components/IndexPage";
 import './Components/style.css'
+import { useEffect } from 'react';
 import ReactGA from 'react-ga4';
 
 const TRACKING_ID = 'G-B2Z721XDXX';
